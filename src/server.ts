e863faf53443ecf504f40d405ba72605bd4a966e
Erick Prado <erickjean36@gmail.com>
2025-06-03 12:10:32 -0400

    feat: base criada

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import routes from './routes';
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+//Rotas
+
+server.use(routes);
+
+server.use((req, res) => {
+    res.status(404).render('404', { title: 'Página não encontrada' });
+});
+
+server.listen(process.env.PORT || 4000);
