1e7447135c9779572ff7ce4723aad96ffa10c2eb
Erick Prado <erickjean36@gmail.com>
2025-06-03 13:14:25 -0400

    feat: menu dinamico

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,40 @@
 import { Request, Response } from "express";
 
+import {createMenuObject} from "../helpers/createMenuObject";
+
 export const home = (req: Request, res: Response) => {
-    res.send('home no controller');
+  res.render("pages/page", {
+    menu: createMenuObject("all"),
+    banner: {
+      title: "Todos os animais",
+      image: "allanimals.jpg",
+    },
+  });
 };
 export const dogs = (req: Request, res: Response) => {
-
+  res.render("pages/page", {
+    menu: createMenuObject("dog"),
+    banner: {
+      title: "Cachorros",
+      image: "banner_dog.jpg",
+    },
+  });
 };
 export const cats = (req: Request, res: Response) => {
-
+  res.render("pages/page", {
+    menu: createMenuObject("cat"),
+    banner: {
+      title: "Gatos",
+      image: "banner_cat.jpg",
+    },
+  });
 };
 export const fishes = (req: Request, res: Response) => {
-
+    res.render("pages/page", {
+    menu: createMenuObject("fish"),
+        banner: {
+        title: "Peixes",
+        image: "banner_fish.jpg",
+        },
+    });
 };
